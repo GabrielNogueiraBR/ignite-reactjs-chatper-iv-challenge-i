e53ce73d812ccc5a61545a8f99f22423c1c91395
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-01-03 19:45:06 -0300

    feat: creating divider on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
+import { Divider, Flex, Text } from "@chakra-ui/react";
 import Banner from "../components/Banner";
 import TravelTypes from "../components/TravelTypes";
 
 export default function Home() {
   return (
-    <>
+    <Flex w="100%" h="100%" position="relative" direction="column">
       <Banner />
       <TravelTypes />
-    </>
+      <Divider
+        alignSelf="center"
+        w="5.625rem"
+        borderColor="gray.600"
+        borderWidth="1px"
+        opacity={1}
+      />
+    </Flex>
   );
 }
